9d948d04341d02359ecfab7c2ae4ddb7c653a1cb
LiamSutton <liam.sutton@my.jcu.edu.au>
2025-09-25 14:22:46 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,6 +1,19 @@
 
-length_of_password = 8
-password = input("Provide a password: ")
-while len(password) <= length_of_password:
-    print("Password must be longer")
-print("*" * len(password))
+def main():
+    length_of_password = 8
+    password = get_password(length_of_password)
+    print_password(password)
+
+
+def print_password(password: str):
+    print("*" * len(password))
+
+
+def get_password(length_of_password: int) -> str:
+    password = input("Provide a password: ")
+    while len(password) <= length_of_password:
+        print("Password must be longer")
+    return password
+
+
+main()
