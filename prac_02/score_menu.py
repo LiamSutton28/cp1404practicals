b05737dcfafcf452f8bc5e15d81108da33201f75
LiamSutton <liam.sutton@my.jcu.edu.au>
2025-09-25 20:43:32 +1000

    Add score_menu program

---
+++ prac_02/score_menu.py
@@ -0,0 +1,47 @@
+""" Score Menu """
+
+MENU = """
+(G)et a valid score
+(P)rint result
+(S)how stars
+(Q)uit
+"""
+
+
+def main():
+    score = float(input("Submit a score: "))
+    while not is_valid(score):
+        print("Invalid score. Need a number between 0 and 100")
+        score = float(input("Submit a score: "))
+    print(MENU)
+    choice = input(">> ").upper()
+    while choice != "Q":
+        if choice == "G":
+            score = float(input("Submit a score: "))
+            while not is_valid(score):
+                print("Invalid score. Need a number between 0 and 100")
+                score = float(input("Submit a score: "))
+        elif choice == "P":
+            print(determine_score_result(score))
+        elif choice == "S":
+            print_stars(score)
+    print("Farewell")
+
+
+def print_stars(score: float):
+    print("*" * round(score))
+
+
+def is_valid(score):
+    return 0 <= score <= 100
+
+
+def determine_score_result(score):
+    if score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+main()
