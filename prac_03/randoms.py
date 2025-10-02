8eceef15e55844eaabe7649541e4b446bf47a586
LiamSutton <liam.sutton@my.jcu.edu.au>
2025-10-02 13:47:15 +1000

    Add randoms program with comments included

---
+++ prac_03/randoms.py
@@ -0,0 +1,13 @@
+""" Randoms practical """
+
+import random
+
+print(random.randint(5, 20))  # line 1
+print(random.randrange(3, 10, 2))  # line 2
+print(random.uniform(2.5, 5.5))  # line 3
+
+"""
+ - I saw a random integer on line 1. The smallest result was 5 and the largest was 20
+ - I saw a random odd number between and including 3 and 9. It could not have produced a 4 as it is even
+ - I saw a decimal between 2.5 and 5.5 but not including them
+"""
