5e9ada673efb9259d9a3a5b2bf2816c822b4e769
LiamSutton <liam.sutton@my.jcu.edu.au>
2025-10-02 15:28:56 +1000

    Add step 3

--- prac_03/files.py
+++ prac_03/files.py
@@ -3,13 +3,19 @@
 FILENAME = "name.txt"
 
 # 1
-provided_name = input("Name: ")
+provided_name = "Liam"  # input("Name: ")
 out_file = open(FILENAME, 'w')
 print(provided_name, file=out_file)
 out_file.close()
 
 # 2
 infile = open(FILENAME)
-found_name = infile.readline().strip()
+found_name = infile.read().strip()
 print(f"Hi {found_name}")
 infile.close()
+
+# 3
+with open("numbers.txt") as in_file:
+    number1 = int(in_file.readline())
+    number2 = int(in_file.readline())
+print(number1 + number2)
