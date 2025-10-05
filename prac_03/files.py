3f6173847a5be8389136556b3df45130b61e6b5f
LiamSutton <liam.sutton@my.jcu.edu.au>
2025-10-05 12:42:49 +1000

    Add comments for understanding

--- prac_03/files.py
+++ prac_03/files.py
@@ -10,13 +10,13 @@ out_file.close()
 
 # 2
 infile = open(FILENAME)
-found_name = infile.read().strip()
+found_name = infile.read().strip() #reads entire file and strips white space
 print(f"Hi {found_name}")
 infile.close()
 
 # 3
 with open("numbers.txt") as in_file:
-    number1 = int(in_file.readline())
+    number1 = int(in_file.readline()) # reads one line of code and stores it
     number2 = int(in_file.readline())
 print(number1 + number2)
 
@@ -24,6 +24,6 @@ print(number1 + number2)
 sum_of_all_lines = 0
 with open("numbers.txt") as in_file:
     for line in in_file:
-        number = int(line)
-        sum_of_all_lines += number
+        number = int(line)      # reads every line in the file and saves it as number
+        sum_of_all_lines += number  # adds number to the total which updates after every line
 print(sum_of_all_lines)
