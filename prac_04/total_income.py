0d9964e2acf786d507c4b3e78d9a57b25e59e4cf
LiamSutton <liam.sutton@my.jcu.edu.au>
2025-10-09 19:39:00 +1000

    Refactor to separate total income calculations into a different function

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -2,6 +2,7 @@
 CP1404/CP5632 Practical
 Starter code for cumulative total income program
 """
+from itertools import accumulate
 
 
 def main():
@@ -12,17 +13,26 @@ def main():
     for month in range(1, number_of_months + 1):
         income = float(input(f"Enter income for month {str(month)}: "))
         incomes.append(income)
+    totals = calculate_cumulative_income(incomes, number_of_months)
+    print_report(incomes, number_of_months, totals)
 
-    print_report(incomes, number_of_months)
 
-
-def print_report(incomes: list[float], number_of_months: int):
+def print_report(incomes: list[float], number_of_months: int, totals: list[float]):
+    """ Print income report function"""
     print("\nIncome Report\n-------------")
-    total = 0
     for month in range(1, number_of_months + 1):
         income = incomes[month - 1]
-        total += income
+        total = totals[month - 1]
         print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+
+
+def calculate_cumulative_income(incomes: list[float], number_of_months: int):
+    accumulative_total = 0
+    totals = []
+    for month in range(1, number_of_months + 1):
+        accumulative_total += incomes[month - 1]
+        totals.append(accumulative_total)
+    return totals
 
 
 main()
