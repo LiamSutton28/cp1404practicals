efcd7d23cb8300983f0d204fb1511a975318d4d1
LiamSutton <liam.sutton@my.jcu.edu.au>
2025-10-09 13:57:13 +1000

    Refactor printing subject details and add appropriate names

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,8 +7,13 @@ FILENAME = "subject_data.txt"
 
 
 def main():
-    data = load_data(FILENAME)
-    print(data)
+    records = load_data(FILENAME)
+    print_subject_details(records)
+
+
+def print_subject_details(records: list):
+    for record in records:
+        print(f"{record[0]} is taught by {record[1]} and has {record[2]} students")
 
 
 def load_data(filename=FILENAME):
