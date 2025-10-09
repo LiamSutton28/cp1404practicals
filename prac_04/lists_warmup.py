0c200f4d72dbb6ea1cea72a14a8e517242d5d369
LiamSutton <liam.sutton@my.jcu.edu.au>
2025-10-09 13:11:11 +1000

    Add statement answers to q1-4

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -13,3 +13,11 @@ numbers[3:4] = [1]
 "3" in numbers = False
 numbers + [6, 5, 3] = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
 """
+# 1
+numbers[0] = 10
+# 2
+numbers[-1] = 1
+# 3
+print(numbers[2:])
+# 4
+print(9 in numbers)
