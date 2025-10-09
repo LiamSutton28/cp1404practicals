389a291bd6686add9ca4a9bff375cb926e2b9e0d
LiamSutton <liam.sutton@my.jcu.edu.au>
2025-10-09 13:50:37 +1000

    Refactor load data to only return the list of records

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -14,16 +14,13 @@ def main():
 def load_data(filename=FILENAME):
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(filename)
+    records = []
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
-        line = line.strip()  # Remove the \n
-        parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
+        parts = line.strip().split(',')  # Remove the \n
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        records.append(parts)
     input_file.close()
+    return records
 
 
 main()
