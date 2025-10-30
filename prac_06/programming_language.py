722f3b727912aecc388a81b3320e86b958ca3657
LiamSutton <liam.sutton@my.jcu.edu.au>
2025-10-30 14:25:33 +1000

    Complete languages program with ProgrammingLanguages class

---
+++ prac_06/programming_language.py
@@ -0,0 +1,16 @@
+"""Class ProgrammingLanguage"""
+
+class ProgrammingLanguage:
+
+    def __init__(self, name, typing, reflection, year):
+        """Initialise a Programming Language object"""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def __str__(self):
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+
+    def is_dynamic(self):
+        return self.typing == "Dynamic"
