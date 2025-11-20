0b15cf02c0ff99a57a0e7f760cc58a05d06e2135
LiamSutton <liam.sutton@my.jcu.edu.au>
2025-11-20 14:15:58 +1000

    Update silver_service_taxi_testto include demo car

--- prac_09/silver_service_test.py
+++ prac_09/silver_service_test.py
@@ -4,7 +4,10 @@ from prac_09.taxi import Taxi
 
 normal_taxi = Taxi("Ranger", 100)
 pricey_taxi = SilverServiceTaxi("Lamborghini", 100, 2)
+test_taxi = SilverServiceTaxi("Lamborghini", 100, 2)
 normal_taxi.drive(100)
 pricey_taxi.drive(100)
+test_taxi.drive(18)
 print(normal_taxi.get_fare())
 print(f"{pricey_taxi.get_fare()} should be equal to {normal_taxi.get_fare() * 2 + 4.5}")
+print(test_taxi.get_fare())
