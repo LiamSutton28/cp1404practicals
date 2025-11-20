0511bdcb1cc8fd7a6fda4c885c4748fd5298a814
LiamSutton <liam.sutton@my.jcu.edu.au>
2025-11-20 14:45:53 +1000

    Add band program

---
+++ prac_09/band.py
@@ -0,0 +1,18 @@
+class Band:
+    def __init__(self, name=""):
+        self.name = name
+        self.musicians = []
+
+    def __str__(self):
+        return f"{self.name} ({self.musicians})"
+
+    def __repr__(self):
+        """Return a string representation of a Band, showing the variables."""
+        return str(vars(self))
+
+    def add(self, musician):
+        self.musicians.append(musician)
+
+    def play(self):
+        for musician in self.musicians:
+            print(musician.play())
