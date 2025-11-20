eb4b37f0cb0e0bea618b24dea2866112f05f430c
LiamSutton <liam.sutton@my.jcu.edu.au>
2025-11-20 15:00:43 +1000

    Update str method

--- prac_09/band.py
+++ prac_09/band.py
@@ -4,11 +4,11 @@ class Band:
         self.musicians = []
 
     def __str__(self):
-        return f"{self.name} ({self.musicians})"
+        return f"{self.name} ({",".join(str(musician) for musician in self.musicians)})"
 
     def __repr__(self):
         """Return a string representation of a Band, showing the variables."""
-        return str(vars(self))
+        return str(self)
 
     def add(self, musician):
         self.musicians.append(musician)
